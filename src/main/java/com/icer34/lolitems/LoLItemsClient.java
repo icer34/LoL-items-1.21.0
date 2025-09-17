9a30453130f5f8af67a7a532fe4e9d26eb70c590
Thomas Desoleil <icer23@Thomass-MacBook-Air.local>
2025-09-17 17:55:07 +0200

    Initial commit

---
+++ src/main/java/com/icer34/lolitems/LoLItemsClient.java
@@ -0,0 +1,10 @@
+package com.icer34.lolitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LoLItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/icer34/lolitems/LoLItemsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.icer34.lolitems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LoLItemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
